f42e05a94ed9e9f23005c42aff4077f911e8bd79
Andri Faqih <realizy@student.telkomuniversity.ac.id>
2025-01-02 09:38:53 +0700

    Add Increment, Decrement on Cart.

--- src/routes/(protected)/cart/+page.svelte
+++ src/routes/(protected)/cart/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
     import type { PageData } from './$types';
     export let data: PageData;
+
+    function incrementQuantity(index: number) {
+        data.carts[index].quantity += 1;
+    }
+
+    function decrementQuantity(index: number) {
+        if (data.carts[index].quantity > 1) {
+            data.carts[index].quantity -= 1;
+        }
+    }
 </script>
 
 <div class="container mx-auto px-4 py-8">
@@ -18,17 +28,21 @@
         </p>
     {/each} -->
     <div class="mt-8 bg-white p-4 rounded-lg">
-        {#each data.carts as item}
+        {#each data.carts as item, index}
             <div class="flex justify-between items-center border-b border-gray-200 py-2">
-                <div class="flex items center space-x-2">
+                <div class="flex items-center space-x-2">
                     <img src={item.product_imageurl} alt={item.product_name} class="w-32 h-32 object-cover rounded-lg" />
                     <div>
                         <p class="font-semibold">{item.product_name}</p>
                         <p class="text-gray-600">Rp {Intl.NumberFormat('id-ID').format(item.product_price)}</p>
-                        <p class="text-gray-600">Quantity: {item.quantity}</p>
+                        <div class="flex items-center space-x-2">
+                            <button on:click={() => decrementQuantity(index)} class="bg-gray-200 px-2 py-1 rounded">-</button>
+                            <div class="px-2 py-1 border border-gray-200 rounded">{item.quantity}</div>
+                            <button on:click={() => incrementQuantity(index)} class="bg-gray-200 px-2 py-1 rounded">+</button>
+                        </div>
                     </div>
                 </div>
-                <div class="flex items center space-x-2">
+                <div class="flex items-center space-x-2">
                 </div>
             </div>
         {/each}
@@ -40,4 +54,16 @@
     <div class="flex justify-end mt-4">
         <button class="bg-[#6c63ff] text-white px-4 py-2 rounded-full hover:bg-purple-600 transition duration-300">Checkout</button>
     </div>
+
+    <!--Modal Prompt delete item-->
+    <div class="fixed inset-0 z-10 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden" id="modal">
+        <div class="bg-white p-4 rounded-lg w-1/3">
+            <p>Are you sure you want to delete this item?</p>
+            <div class="flex justify-end mt-4 gap-x-1">
+                <button class="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600 transition duration-300">Delete</button>
+                <button class="bg-gray-200 text-gray-800 px-4 py-2 rounded-full hover:bg-gray-300 transition duration-300">Cancel</button>
+            </div>
+        </div>
+    </div>
+    
 </div>
